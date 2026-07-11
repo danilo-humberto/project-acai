d0fa47d0f60d7209ff36e6dccaab0b885bfd68ca
Danilo <danilohumberto12@hotmail.com>
2026-07-10 21:08:14 -0300

    Refactor syrup options to use consistent quotation marks and add "Caramelo" and "Banana caramelizada" syrup options

--- src/data/syrups.ts
+++ src/data/syrups.ts
@@ -1,34 +1,46 @@
-import type { SyrupOption } from '../types/order'
+import type { SyrupOption } from "../types/order";
 
 export const syrups: SyrupOption[] = [
   {
-    id: 'sem-calda',
-    name: 'Sem calda',
-    description: 'Sem cobertura extra',
+    id: "sem-calda",
+    name: "Sem calda",
+    description: "Sem cobertura extra",
     price: 0,
   },
   {
-    id: 'menta',
-    name: 'Menta',
-    description: 'Refrescante',
+    id: "menta",
+    name: "Menta",
+    description: "Refrescante",
     price: 0,
   },
   {
-    id: 'morango',
-    name: 'Morango',
-    description: 'Frutada e brilhante',
+    id: "morango",
+    name: "Morango",
+    description: "Frutada e brilhante",
     price: 0,
   },
   {
-    id: 'chocolate',
-    name: 'Chocolate',
-    description: 'Clássica e cremosa',
+    id: "chocolate",
+    name: "Chocolate",
+    description: "Clássica e cremosa",
     price: 0,
   },
   {
-    id: 'leite-condensado',
-    name: 'Leite condensado',
-    description: 'O clássico do açaí',
+    id: "leite-condensado",
+    name: "Leite condensado",
+    description: "O clássico do açaí",
     price: 0,
   },
-]
+  {
+    id: "caramelo",
+    name: "Caramelo",
+    description: "Doce e cremoso",
+    price: 0,
+  },
+  {
+    id: "banana-caramelizada",
+    name: "Banana caramelizada",
+    description: "Doce e cremoso",
+    price: 0,
+  },
+];
