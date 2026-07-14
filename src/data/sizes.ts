3dcefd61c17ebedab6a4315a1f1ac743753a05c0
Danilo Humberto <danilohumberto12@hotmail.com>
2026-07-14 09:17:57 -0300

    refactor: standardize formatting and improve descriptions in sizes data

--- src/data/sizes.ts
+++ src/data/sizes.ts
@@ -1,48 +1,48 @@
-import barcaImage from '../assets/images/barca.png'
-import product300Image from '../assets/images/product-300.png'
-import product500Image from '../assets/images/product-500.png'
-import product700Image from '../assets/images/product-700.png'
-import type { SizeOption } from '../types/order'
+import barcaImage from "../assets/images/barca.png";
+import product300Image from "../assets/images/product-300.png";
+import product500Image from "../assets/images/product-500.png";
+import product700Image from "../assets/images/product-700.png";
+import type { SizeOption } from "../types/order";
 
 export const sizes: SizeOption[] = [
   {
-    id: 'p',
-    name: 'P',
-    volume: 'P',
-    description: 'Pote pequeno',
+    id: "p",
+    name: "P",
+    volume: "P",
+    description: "Pote pequeno",
     price: 8,
     image: product300Image,
   },
   {
-    id: 'm',
-    name: 'M',
-    volume: 'M',
-    description: 'Pote médio',
+    id: "m",
+    name: "M",
+    volume: "M",
+    description: "Pote médio",
     price: 10,
     image: product500Image,
   },
   {
-    id: 'g',
-    name: 'G',
-    volume: 'G',
-    description: 'Pote grande',
+    id: "g",
+    name: "G",
+    volume: "G",
+    description: "Pote grande",
     price: 12,
     image: product700Image,
   },
   {
-    id: 'barca-p',
-    name: 'Barca P',
-    volume: 'Barca P',
-    description: 'Barca pequena',
+    id: "barca-p",
+    name: "Barca P",
+    volume: "Barca P",
+    description: "Serve 1-2 pessoas",
     price: 20,
     image: barcaImage,
   },
   {
-    id: 'barca-m',
-    name: 'Barca M',
-    volume: 'Barca M',
-    description: 'Barca média',
+    id: "barca-m",
+    name: "Barca M",
+    volume: "Barca M",
+    description: "Serve 2-4 pessoas",
     price: 30,
     image: barcaImage,
   },
-]
+];
